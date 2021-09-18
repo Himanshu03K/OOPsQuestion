e57a5e4008d91ee732bda33eb0a56786078b79d4
Himanshu Kushwaha <kushwaha0312@gmail.com>
2021-09-19 00:50:46 +0530

    Array

--- src/Shape.java
+++ src/Shape.java
@@ -1,3 +1,9 @@
+/*
+Q1) Write a program to create a class named shape. It should contain 2 methods, draw() and erase()
+     that prints “Drawing Shape” and “Erasing Shape” respectively.
+ */
+
+
 import java.util.Scanner;
 
     public class Shape {
